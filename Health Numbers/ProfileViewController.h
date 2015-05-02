bcc07a211a3977be67d99056964d6f93f2dd3cae
alfonsopintos <alfonsopintos@gmail.com>
2015-05-02 11:16:02 -0400

    fb login and initial profile page layout

---
+++ Health Numbers/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Health Numbers
+//
+//  Created by Alfonso Pintos on 5/2/15.
+//  Copyright (c) 2015 Meme Menu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+    @property(nonatomic) NSDictionary *userProfile;
+@end
--- Health Numbers/ViewController.h
+++ Health Numbers/ViewController.h
@@ -7,8 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import "ProfileViewController.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <FBLoginViewDelegate>
 
 
 @end
